66ec31a538f4ce7c9fd28d5416e044bd4087c6df
aradee3-commits <aradee3@gmail.com>
2025-08-22 23:45:31 +0300

    Add Color.kt file

---
+++ app/src/main/java/com/example/noor/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.noor.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryLight = Color(0xFF6750A4)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryDark = Color(0xFFD0BCFF)
+val OnPrimaryDark = Color(0xFF381E72)
