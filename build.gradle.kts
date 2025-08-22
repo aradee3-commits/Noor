6a8731d06d448b8cd06c2eb227d1c54c1d0f96ce
aradee3-commits <aradee3@gmail.com>
2025-08-22 23:13:46 +0300

    Add root and app Gradle Kotlin DSL build scripts and ProGuard rules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    // يمكن إضافة Plugins لاحقاً (مثل detekt أو ktlint)
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
